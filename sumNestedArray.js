31234787cca5cf3781c734bf906f76ce1ffd66f3
Priyanka Koneru <koneru.priyanka@gmail.com>
2020-02-11 13:17:11 -0800

    functionality for sumNestedArray function

--- sumNestedArray.js
+++ sumNestedArray.js
@@ -1,15 +1,22 @@
-//sumNestedArray(arr);
+// Create a function sumNestedArray with array as an argument
+function sumNestedArray(arr) {
+  // Expected is an array, so lets create a sumArray and assign it to [].
+  let sumArr = [];
+  // Create a sum variable to store the sum of individual nested array items
+  let sum = 0;
+
+  // loop through the input array
+  for(let i = 0; i < arr.length; i++) {
+      // create a new arr1 variable to store the individual nested array
+      let arr1 = arr[i];
+      // create a value variable with .reduce() method on each element of the array, resulting in a single output value.
+      let value = arr1.reduce((sum, element) => sum + element);
+      // Push that individual value into the sumArr
+      sumArr.push(value);
+  }
+  //return sumArr
+  return sumArr;
+}
 //const arr = [[12,12],[6,6]];
-//sumNestedArray(arr); //  [24, 12]
-
-// Here since the input and output are not clear, had to get them clarified first
-// Expected is an array, so lets create a sumArray and assign it to [].
-// loop through the outer array first which in this case would be [[12,12],[6,6]]
-// then loop through the individual inner array's in this case it would be [12,12] and [6,6]
-// then loop through the array [12,12] based on the length and sum up the individual array elements
-// then keep pushing the sum to the sumArray, once done
-//return the sumArray
-
-
-
+//console.log(sumNestedArray(arr));//[24, 12]
 
