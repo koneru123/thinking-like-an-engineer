67eefab89e2578b5fed04b613bfc681655e8c7b6
Priyanka Koneru <koneru.priyanka@gmail.com>
2020-02-11 10:43:22 -0800

    reverse string functionality

--- reverseString.js
+++ reverseString.js
@@ -1,7 +1,22 @@
-// first lets understand the input and output, in this case it is a string
-// create a variable to store that string
-// return that variable
-// then loop through the string with i as string.length, till i becomes 0, keep incrementing i
-// print it from reverse, concatenate it to the variable created above
+// Create a function reverseString, which has a word as an argument
+function reverseString(word) {
+  // first lets understand the input and output, in this case it is a string
+  // create a variable reverse to store that string
+  let reverse = '';
+  // then loop through the string with i as string.length-1, till i >= 0, keep decrementing i
+  for(let i = word.length-1; i >= 0 ; i--) {
+    // print it from reverse, concatenate it to the variable created above
+    reverse += word[i];
+  }
+  // return that variable
+  return reverse;
+}
+
+//console.log(reverseString('priya')); // return ayirp
+
+
+
+
+
 
 
