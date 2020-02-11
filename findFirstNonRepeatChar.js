20511507065658df240361f4aa1e51815b259831
Priyanka Koneru <koneru.priyanka@gmail.com>
2020-02-11 10:16:06 -0800

    Pseudo code for findFirstNonRepeatChar

--- findFirstNonRepeatChar.js
+++ findFirstNonRepeatChar.js
@@ -1,1 +1,9 @@
-
+// Lets understand the input and expected output
+// Input is a word and expected is to find the first character that is not repeated.
+// First create a string variable(nonRepeatChar) and assign it to empty string
+// Assign the first character of the word to nonRepeatChar
+// Loop through that word with i = 1, till the length of the word
+// See if first character of the word is not equal to word[i], assign it to nonRepeatChar variable if it is
+// return that variable(nonRepeatChar)
+// if not assign word[i] to nonRepeatChar variable and repeat this step
+// return that variable(nonRepeatChar)
