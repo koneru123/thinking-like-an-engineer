6a4956809b885a76f01b10e1ad02750487c9efe1
Priyanka Koneru <koneru.priyanka@gmail.com>
2020-02-11 15:40:49 -0800

    code transitional steps for findFirstNonRepeatChar

--- findFirstNonRepeatChar.js
+++ findFirstNonRepeatChar.js
@@ -1,3 +1,32 @@
+//Transitional Steps
+// accept input as a string
+  // 'abaaba' | 'abacabad' | 'abaabad'
+
+// output should be the first non repeated character or null if no such character
+  // null | 'c' | 'd'
+
+/*
+  1. Create a function
+  2. Create a nonRepeatChar variable and assign it to empty string
+  3. Create an empty object variable
+  4. Iterate through the word, check if the key exists in object, if exists increment the value of it if not add value as 1
+
+  Case 1:-
+  input is 'abacabad', output should be 'c'
+
+  iter    currentword  check in object | count | output
+  0       a            does not exist  | 1     | {a:1}
+  1       b            does not exist  | 1     | {a:1, b:1}
+  2       a            exists          | 2     | {a:2, b:1}
+  3       c            does not exist  | 1     | {a:2. b:1, c:1}
+  4       a            exists          | 3     | {a:3, b:1, c:1}
+  5       b            exists          | 2     | {a:3, b:2, c:1}
+  6       a            exists          | 4     | {a:4, b:2, c:1}
+  7       d            does not exist  | 1     | {a:4, b:2, c:1, d:1}
+
+  5. After the object is returned we then check for first key whose value is 1 and return that, if all the keys have values > 1 return null
+*/
+
 // Create a function findFirstNonRepeatChar() which accepts word as an argument
 function findFirstNonRepeatChar(word) {
   // Lets understand the input and expected output
@@ -34,6 +63,6 @@ function findFirstNonRepeatChar(word) {
   return nonRepeatChar;
 }
 
-//console.log(findFirstNonRepeatChar('abaaba')); returns null
-//console.log(findFirstNonRepeatChar('abacabad')); returns c
-//console.log(findFirstNonRepeatChar('abaabad')); returns d
+//console.log(findFirstNonRepeatChar('abaaba')); //returns null
+//console.log(findFirstNonRepeatChar('abacabad')); //returns c
+//console.log(findFirstNonRepeatChar('abaabad')); //returns d
