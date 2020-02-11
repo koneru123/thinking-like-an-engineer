ad39bd863f25b092c266fbc33cbb4c18b64460e2
Priyanka Koneru <koneru.priyanka@gmail.com>
2020-02-11 15:22:13 -0800

    added transitional steps

--- reverseString.js
+++ reverseString.js
@@ -1,3 +1,28 @@
+//Transitional Steps
+// accept input as a string
+  // 'priya' | 'apple'
+
+// output reverse input of a string
+  // 'ayiyp' | 'elppa'
+
+/*
+  1. first
+  2. Iterate through input string backwards, and concatenate the current value to the result string.
+  3. return result string
+
+  input 'priya', output 'ayiyp'
+  word length = 5-1 = 4
+
+  iter    current character from input | output
+  4       a         | '' + 'a'  = 'a'
+  3       y         | 'a' + 'y' = 'ay'
+  2       i         | 'ay' + 'i' = 'ayi'
+  1       r         | 'ayi' + 'r' = 'ayir'
+  0       p         | 'ayir' + 'p' = 'ayirp'
+
+  return output
+*/
+
 // Create a function reverseString, which has a word as an argument
 function reverseString(word) {
   // first lets understand the input and output, in this case it is a string
@@ -13,6 +38,7 @@ function reverseString(word) {
 }
 
 //console.log(reverseString('priya')); // return ayirp
+
 
 
 
