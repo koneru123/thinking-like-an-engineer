e0479e39f61eab04647e55fb46da444320db212c
Priyanka Koneru <koneru.priyanka@gmail.com>
2020-02-11 10:35:49 -0800

    Pseudocode for sumNestedArray problem

--- sumNestedArray.js
+++ sumNestedArray.js
@@ -1,1 +1,15 @@
+//sumNestedArray(arr);
+//const arr = [[12,12],[6,6]];
+//sumNestedArray(arr); //  [24, 12]
+
+// Here since the input and output are not clear, had to get them clarified first
+// Expected is an array, so lets create a sumArray and assign it to [].
+// loop through the outer array first which in this case would be [[12,12],[6,6]]
+// then loop through the individual inner array's in this case it would be [12,12] and [6,6]
+// then loop through the array [12,12] based on the length and sum up the individual array elements
+// then keep pushing the sum to the sumArray, once done
+//return the sumArray
+
+
+
 
