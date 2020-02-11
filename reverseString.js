8f7af96728b2f008b420a7236149e22678fa1d2d
Priyanka Koneru <koneru.priyanka@gmail.com>
2020-02-11 10:07:55 -0800

    pseudo code for reverseString

--- reverseString.js
+++ reverseString.js
@@ -1,1 +1,7 @@
+// first lets understand the input and output, in this case it is a string
+// create a variable to store that string
+// return that variable
+// then loop through the string with i as string.length, till i becomes 0, keep incrementing i
+// print it from reverse, concatenate it to the variable created above
+
 
